1598c96e61186ef6d66538d4e07636f7ed09643f
sanjaykumar2411 <sanjaykumaruxui@gmail.com>
2025-11-09 02:39:50 +0530

    FIRST

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3C37FF',
+        dark: '#0A0A1F',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
